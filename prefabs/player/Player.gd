2effe7b036c4eb4b8b60f6ee3fcd7688ea00434f
creepersaur <69436784+creepersaur@users.noreply.github.com>
2025-04-23 19:34:05 +0530

    player
    - rework player movement
    - file structure update

---
+++ prefabs/player/Player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+const SPEED: float = 200;
+const DRAG: float = 0.8;
+const JUMP_POWER: float = 200;
+const GRAVITY: float = 7;
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event:InputEvent)->void:
+	if Input.is_action_just_pressed("Tab"):
+		match Input.mouse_mode:
+			Input.MOUSE_MODE_CAPTURED: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			_: Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	var direction := Vector3(
+		Input.get_axis("left", "right"),
+		0,
+		Input.get_axis("forward", "backward"),
+	).normalized()
+	
+	var rotated_direction = -direction
+	
+	velocity += SPEED * delta * rotated_direction
+	velocity *= Vector3(DRAG, 1, DRAG)
+	move_and_slide()
