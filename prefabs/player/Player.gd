48ecd3f874fdaec4072caa273c84d5b1e232a596
Supergames207 <157420613+Supergames207@users.noreply.github.com>
2025-04-23 15:39:04 +0100

    Basic Jump

--- prefabs/player/Player.gd
+++ prefabs/player/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 const SPEED: float = 160;
 const DRAG: float = 0.8;
-const JUMP_POWER: float = 200;
+const JUMP_POWER: float = 4;
 const GRAVITY: float = 7;
 
 @onready var X_rot = $X_rotation
@@ -28,4 +28,6 @@ func _physics_process(delta: float) -> void:
 	velocity += SPEED * delta * rotated_direction
 	velocity *= Vector3(DRAG, 1, DRAG)
 	velocity.y += -GRAVITY*delta
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += JUMP_POWER
 	move_and_slide()
