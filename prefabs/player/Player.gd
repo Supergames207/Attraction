9bc499f9364a42d55c8b939f4bf1a6b2367d1047
creepersaur <69436784+creepersaur@users.noreply.github.com>
2025-04-23 21:38:19 +0530

    better jumping + no headbob
    Made JUMPING BETTER AND REMOVED HEADBOB

--- prefabs/player/Player.gd
+++ prefabs/player/Player.gd
@@ -2,14 +2,9 @@ extends CharacterBody3D
 
 const SPEED: float = 160;
 const DRAG: float = 0.8;
-const JUMP_POWER: float = 4;
-const GRAVITY: float = 7;
+const JUMP_POWER: float = 10;
+const GRAVITY: float = 30;
 
-#Head Bob
-const default_camera_pos := Vector3(0,0.5,0)
-const BOB_FREQ := 2
-const BOB_AMP := 0.08
-var bob_t := 0.
 @onready var X_rot := $X_rotation
 
 func _ready() -> void:
@@ -31,16 +26,10 @@ func _physics_process(delta: float) -> void:
 	var rotated_direction := -direction.rotated(Vector3.UP, X_rot.rotation.y)
 	velocity += SPEED * delta * rotated_direction
 	velocity *= Vector3(DRAG, 1, DRAG)
-	if not is_on_floor():
-		velocity.y += -GRAVITY*delta
-	elif Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y += JUMP_POWER
-	bob_t += delta*velocity.length()*float(is_on_floor())
-	X_rot.position = head_bob(bob_t)
+	if is_on_floor():
+		velocity.y = 0	
+		if Input.is_action_just_pressed("jump"):
+			velocity.y += JUMP_POWER
+	else: velocity.y += -GRAVITY*delta
+	
 	move_and_slide()
-
-func head_bob(time:float)->Vector3:
-	var pos := Vector3.ZERO
-	pos.y = sin(time*BOB_FREQ)*BOB_AMP
-	pos.x = cos(time*BOB_FREQ/2)*BOB_AMP
-	return pos+default_camera_pos
