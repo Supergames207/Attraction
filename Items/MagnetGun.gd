c3d86d90b02377ae75555d12db4b5513cc060c6b
Supergames207 <157420613+Supergames207@users.noreply.github.com>
2025-04-23 19:57:01 +0100

    Magnet Fixes

--- Items/MagnetGun.gd
+++ Items/MagnetGun.gd
@@ -1,16 +1,16 @@
 extends Node3D
 var pull := false
-var on := false
-var magnet_power := 0.2
+var magnet_power := 0.25
 
 func _input(e:InputEvent)->void:
 	if not e is InputEventMouseButton:return
 	var event:InputEventMouseButton = e
 	if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		on = not on
+		$MagnetReach.monitoring = not $MagnetReach.monitoring
 
 
 func _physics_process(_delta:float)->void:
+	if not $MagnetReach.monitoring: return
 	for rigid:Node3D in $MagnetReach.get_overlapping_bodies():
 		if not rigid is RigidBody3D:continue
 		var error :Vector3= global_position-rigid.global_position
