746e4333c5f478cf13c31865a312af62c8e2fe4c
Supergames207 <157420613+Supergames207@users.noreply.github.com>
2025-04-24 16:25:36 +0100

    Fixes

---
+++ Components/MovementComponent.gd
@@ -0,0 +1,7 @@
+@tool
+extends BaseComponent
+class_name MovementComponent
+
+func get_custom_class_name()->String:
+	return "MovementComponent"
+
--- Creatures/BaseCreature.gd
+++ Creatures/BaseCreature.gd
@@ -1,12 +1,13 @@
 @tool
-extends Node3D
+extends RigidBody3D
 class_name BaseCreature
 
 @export var components :Dictionary[String,BaseComponent]:
 	set(value):
 		for key:String in value.keys():
-			if not "get_custom_class_name" in value[key]: continue
+			if not "get_custom_class_name" in value[key] or value[key].get_custom_class_name() == key: continue
 			#Replace the key for the custom class name
 			value[value[key].get_custom_class_name()] = value[key]
 			value.erase(key)
+		
 		components = value
