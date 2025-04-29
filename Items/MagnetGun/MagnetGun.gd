dc62bd62daaacb424356c6a66c0907dc479ad1ab
creepersaur <69436784+creepersaur@users.noreply.github.com>
2025-04-30 00:31:38 +0530

    Magnet gun pull + push

--- Items/MagnetGun/MagnetGun.gd
+++ Items/MagnetGun/MagnetGun.gd
@@ -1,27 +1,32 @@
 extends Node3D
 
 @export var magnet_power := 4
-var pulling := false
+var pulling := 0.0
 
 @onready var magnet_area: Area3D = $MagnetArea
 @onready var gun_front: Node3D = $GunFront
 
 func _physics_process(delta: float) -> void:
-	pulling = Input.is_action_pressed("magnet_pull")
-	pull_objects(delta)
+	pulling = Input.get_axis("magnet_push", "magnet_pull")
+	apply_object_force(delta)
 
-func pull_objects(delta: float):
+func apply_object_force(delta: float) -> void:
 	if not pulling: return
-	var bodies = magnet_area.get_overlapping_bodies()
+	var bodies: Array[Node3D] = magnet_area.get_overlapping_bodies()
 	
 	for i: Node3D in bodies:
 		if i.name == "Player": continue
 		if i is not RigidBody3D: continue
 		
-		var displacement = gun_front.global_position - i.global_position
-		var acceleration = displacement * magnet_power / displacement.length()
-		var dampening = 0.7
+		var displacement := gun_front.global_position - i.global_position
+		var acceleration := displacement * magnet_power / displacement.length()
+		var pull_dampening := 0.7
+		var push_dampening := 0.8
 		
 		var rb: RigidBody3D = i
-		rb.linear_velocity += acceleration * delta * 60
-		rb.linear_velocity *= dampening
+		rb.linear_velocity += pulling * acceleration * delta * 60
+		
+		if pulling > 0:
+			rb.linear_velocity *= pull_dampening
+		else:
+			rb.linear_velocity *= push_dampening
