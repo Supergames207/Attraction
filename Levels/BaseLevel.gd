638abb864bab3fc4caf09cf2e85ee43e9686fd48
Supergames207 <157420613+Supergames207@users.noreply.github.com>
2025-05-01 15:36:20 +0100

    Levels improvements

--- Levels/BaseLevel.gd
+++ Levels/BaseLevel.gd
@@ -8,19 +8,19 @@ signal changed
 
 
 func _ready() -> void:
-    changed.connect(update)
-    
+	changed.connect(update)
+	
 func update() -> void:
-    var level_finsihed :bool = len(enemies_holder.get_children())==0
-    
-    if not level_finsihed: return
+	var level_finsihed :bool = len(enemies_holder.get_children())==0
+	
+	if not level_finsihed: return
 
-    for button:DoorButton in buttons_holder.get_children():
-        if button.active: continue
-        level_finsihed = false
+	for button:DoorButton in buttons_holder.get_children():
+		if button.active: continue
+		level_finsihed = false
 
-    if level_finsihed:
-        finish_level()
-    
+	if level_finsihed:
+		finish_level()
+	
 func finish_level() -> void:
-    print("Level Finished")
+	print("Level Finished")
--- Levels/Objects/Button.gd
+++ Levels/Objects/Button.gd
@@ -1,24 +1,23 @@
 class_name DoorButton extends StaticBody3D
 
 var active := false
-var hitbox := Area3D.new()
+
+@onready var hitbox := %HitBox
 
 func _ready() -> void:
-    var col_shape := CollisionShape3D.new()
-    col_shape.shape = BoxShape3D.new()
-    col_shape.shape.size = Vector3(1,1,1)
-
-    add_child(hitbox)
-    hitbox.add_child(col_shape)
-
     hitbox.body_entered.connect(body_entered)
     hitbox.body_exited.connect(body_exited)
 
+func _process(delta:float) -> void:
+    if not active:
+        %Active.position = lerp(%Active.position,Vector3(0,0.125,0),delta*2)
+    else:
+         %Active.position = lerp(%Active.position,Vector3.ZERO,delta*2)
 
-func body_entered() -> void:
+func body_entered(_body:Node) -> void:
     active = len(hitbox.get_overlapping_bodies()) > 0
     owner.changed.emit()
 
-func body_exited() -> void:
+func body_exited(_body:Node) -> void:
     active = len(hitbox.get_overlapping_bodies()) > 0
     owner.changed.emit()
