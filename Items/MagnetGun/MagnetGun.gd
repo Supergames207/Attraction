7b5a237cc6b26b4eb44000b4ed4ffd00e379f636
Supergames207 <157420613+Supergames207@users.noreply.github.com>
2025-05-01 13:44:08 +0100

    Revert "Fixing merge?"
    This reverts commit f3c1c6aad151ecef2fbe0004bbd2b1d6a5191869.

--- Items/MagnetGun/MagnetGun.gd
+++ Items/MagnetGun/MagnetGun.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
 @export var magnet_power := 4
-var pulling := 0.0
+var enabled := false
+var pulling := false
 
 @onready var magnet_area: Area3D = $MagnetArea
 @onready var gun_front: Node3D = $GunFront
@@ -14,26 +15,24 @@ func _input(e:InputEvent)->void:
 		pulling = not pulling
 
 func _physics_process(delta: float) -> void:
-	pulling = Input.get_axis("magnet_push", "magnet_pull")
-	apply_object_force(delta)
+	enabled = Input.is_action_pressed("magnet_enable")
+	pull_objects(delta)
 
-func apply_object_force(delta: float) -> void:
-	if not pulling: return
-	var bodies: Array[Node3D] = magnet_area.get_overlapping_bodies()
+func pull_objects(delta: float)->void:
+	if not enabled: return
+	var bodies := magnet_area.get_overlapping_bodies()
 	
 	for i: Node3D in bodies:
 		if i.name == "Player": continue
 		if i is not RigidBody3D: continue
 		
 		var displacement := gun_front.global_position - i.global_position
-		var acceleration := displacement * magnet_power / displacement.length()
-		var pull_dampening := 0.7
-		var push_dampening := 0.8
+		var acceleration := (displacement * magnet_power / (displacement.length()**2)) * (int(pulling)*2-1)
+		var dampening := 0.7
 		
 		var rb: RigidBody3D = i
-		rb.linear_velocity += pulling * acceleration * delta * 60
-		
-		if pulling > 0:
-			rb.linear_velocity *= pull_dampening
-		else:
-			rb.linear_velocity *= push_dampening
+
+		#rb.apply_central_force(acceleration*12)
+
+		rb.linear_velocity += acceleration * delta * 60
+		rb.linear_velocity *= dampening
