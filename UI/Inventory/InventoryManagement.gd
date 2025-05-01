383f35ccf99f05a73ad351d4b46df7fbf46c6357
Supergames207 <157420613+Supergames207@users.noreply.github.com>
2025-05-01 15:05:51 +0100

    Test?

--- UI/Inventory/InventoryManagement.gd
+++ UI/Inventory/InventoryManagement.gd
@@ -3,18 +3,16 @@ class_name InventoryManagement
 
 @export var default_position: Vector3 = Vector3(0.75, -0.5, -0.75)
 
-
 @onready var camera_3d: Camera3D = %Camera3D
-#@onready var slot_container: HBoxContainer = get_root().get_node("%S_Inventory/%SlotContainer")
-@onready var slot_container: HBoxContainer = get_root().get_node("%SlotContainer")
+@onready var slot_container: HBoxContainer = get_root().get_node("%S_Inventory/%SlotContainer")
 
 var current_slot: PanelContainer = null
 var slot_equipped: int = -1
 var inventory_data := load("res://UI/Inventory/InventoryData.tres")
-# var slot_themes: Dictionary[String, Theme] = {
-# 	default = preload("res://UI/Inventory/Themes/slot_default.tres"),
-# 	equipped = preload("res://UI/Inventory/Themes/slot_equipped.tres"),
-# }
+var slot_themes: Dictionary[String, Theme] = {
+	default = preload("res://UI/Inventory/Themes/slot_default.tres"),
+	equipped = preload("res://UI/Inventory/Themes/slot_equipped.tres"),
+}
 
 func _input(e: InputEvent) -> void:
 	if not e is InputEventKey: return
@@ -44,19 +42,18 @@ func load_item() -> void:
 	
 	var new_item: Node = inventory_data.slotdatas[slot_equipped].item.scene.instantiate()
 	camera_3d.add_child(new_item)
-
 	new_item.position = default_position
 
 func update_slot() -> void:
-	#if current_slot != null:
-		#current_slot.theme = slot_themes.default
+	if current_slot != null:
+		current_slot.theme = slot_themes.default
 	
 	if slot_equipped < 0:
 		current_slot = null
 		return
 	
-	var slot: PanelContainer = PanelContainer.new()#slot_container.get_node("slot_%s" % slot_equipped)
+	var slot: PanelContainer = slot_container.get_node("slot_%s" % slot_equipped)
 	if slot == null: return
 
 	current_slot = slot
-	#slot.theme = slot_themes.equipped
+	slot.theme = slot_themes.equipped
